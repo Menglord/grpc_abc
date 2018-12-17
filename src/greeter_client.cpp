8bc45324c28efa3a7e3ddf6da4d92f3c546c4b1c
zhangmeng <zhangmeng@forwardx.com>
2018-12-17 15:10:47 +0800

    init commit

---
+++ src/greeter_client.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <grpcpp/grpcpp.h>
+
+#include "helloworld.grpc.pb.h"
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+
+class GreeterClient {
+public:
+	GreeterClient(std::shared_ptr<Channel> channel) : stub_(Greeter::NewStub(channel)) {}
+	std::string SayHello(const std::string& user) {
+		HelloRequest request;
+		request.set_name(user);
+		HelloReply reply;
+		ClientContext context;
+		Status status = stub_->SayHello(&context, request, &reply);
+		if (status.ok()) {
+			return reply.message();
+		} else {
+			std::cout << status.error_code() << ": " << status.error_message() << std::endl;
+			return "RPC Failed";
+		}
+	}
+
+private:
+	std::unique_ptr<Greeter::Stub> stub_;
+};
+
+int main(int argc, char** argv){
+	GreeterClient greeter(grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials()));
+	std::string user("forwardx");
+	std::string reply = greeter.SayHello(user);
+	std::cout << "Greeter received: " << reply << std::endl;
+	return 0;
+}
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <grpcpp/grpcpp.h>
+
+#include "helloworld.grpc.pb.h"
+
+int main(int argc, char** argv){
+	std::cout << "Hello GRPC" << std::endl;
+}
